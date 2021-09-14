eb0b241f4c39dd1c77796aba3f33cd81397752ca
johnroutledge <john_routledge@hotmail.com>
2021-09-14 22:37:29 +0000

    connect to MongoDB

--- app.py
+++ app.py
@@ -1,16 +1,29 @@
 import os
-from flask import Flask
+from flask import (
+    Flask, flash, render_template, redirect,
+    request, session, url_for)
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
 if os.path.exists("env.py"):
     import env
 
 
 app = Flask(__name__)
 
+app.config["MONGO_DBNAME"] = os.environ.get("MOGNO_DBNAME")
+app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
+app.secret_key = os.environ.get("SECRET_KEY")
+
+mongo = PyMongo(app)
+
 
 @app.route("/")
-
+@app.route("/get_currencies")
+def get_currencies():
+    currencies = mongo.db.currencies.find()
+    return render_template("currencies.html", currencies = currencies)
 
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
         port=int(os.environ.get("PORT")),
-        debug=True) #change to False before submitting
+        debug=True)  #change to False before submitting
